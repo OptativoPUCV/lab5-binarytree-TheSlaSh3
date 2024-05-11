dd5d426ab803abf034321c8e4dd6747ce17d7cc1
Your Name <you@example.com>
2024-05-11 22:32:54 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -92,12 +92,11 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
 }
 
 TreeNode * minimum(TreeNode * x){
-    TreeNode * aux = x;
-    while (aux->left != NULL)
+    while (x->left != NULL)
     {
-        aux = aux->left;
+        x = x->left;
     }
-    return aux;
+    return x;
 }
 
 /*5.- Implemente la función void removeNode(TreeMap * tree, TreeNode* node). Esta función elimina el nodo node del árbol tree. Recuerde que para eliminar un node existen 3 casos: Nodo sin hijos: Se anula el puntero del padre que apuntaba al nodo Nodo con un hijo: El padre del nodo pasa a ser padre de su hijo Nodo con dos hijos: Descienda al hijo derecho y obtenga el menor nodo del subárbol (con la función minimum). Reemplace los datos (key,value) de node con los del nodo "minimum". Elimine el nodo minimum (para hacerlo puede usar la misma función removeNode)*/
